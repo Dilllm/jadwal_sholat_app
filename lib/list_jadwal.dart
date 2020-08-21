e57b23bafbf6ef40066a8621d2931bd29b36348f
Adil <Adilmutaqin777@gmail.com>
2020-08-21 09:12:15 +0700

    Initial commit

---
+++ lib/list_jadwal.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:jadwal_sholat_app/text_style.dart';
+
+import 'model/ResponseJadwal.dart';
+
+class ListJadwal extends StatelessWidget {
+  ResponseJadwal data;
+
+  ListJadwal(this.data);
+
+  Widget containerWaktu(String waktu, String jam) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        height: 70.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5.0)],
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Color(0xFFF06292), Color(0xff3fada8)])),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(waktu, style: styleListText),
+            Text(jam, style: styleListText),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        containerWaktu(
+          "Subuh",
+          data.results.datetime[0].times.Fajr.toUpperCase()
+        ),
+        containerWaktu(
+            "Dzuhur",
+            data.results.datetime[0].times.Dhuhr.toUpperCase()
+        ),
+        containerWaktu(
+            "Ashar",
+            data.results.datetime[0].times.Asr.toUpperCase()
+        ),
+        containerWaktu(
+            "Maghrib",
+            data.results.datetime[0].times.Maghrib.toUpperCase()
+        ),
+        containerWaktu(
+            "Isya",
+            data.results.datetime[0].times.Isha.toUpperCase()
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:jadwal_sholat_app/header_content.dart';
+import 'package:jadwal_sholat_app/model/ResponseJadwal.dart';
+
+import 'list_jadwal.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyHomeScreen(),
+  ));
+}
+
+class MyHomeScreen extends StatefulWidget {
+  @override
+  _MyHomeScreenState createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen> {
+  TextEditingController _locationController = TextEditingController();
+
+  Future<ResponseJadwal> getJadwal({String location}) async {
+    String url =
+        'https://api.pray.zone/v2/times/today.json?city=$location&school=9';
+    final response = await http.get(url);
+    final jsonResponse = json.decode(response.body);
+    return ResponseJadwal.fromJsonMap(jsonResponse);
+  }
+
+  void iniState() {
+    if (_locationController.text.isEmpty || _locationController.text == null) {
+      _locationController.text = 'Bogor';
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final header = Stack(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.width - 120,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30.0),
+                  bottomLeft: Radius.circular(30.0)),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 6.0,
+                    offset: Offset(0.0, 2.0),
+                    color: Colors.black26)
+              ],
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://images.unsplash.com/flagged/photo-1558113118-e42e558b352a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'))),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Tooltip(
+                message: 'Ubah Lokasi',
+                child: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.location_on),
+                    onPressed: () {
+                      _showDialogEditLocation(context);
+                    }),
+              ),
+            ],
+          ),
+        ),
+        FutureBuilder(
+          future: getJadwal(
+              location: _locationController.text.toLowerCase().toString()),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return HeaderContent(snapshot.data);
+            } else if (snapshot.hasError) {
+              print(snapshot.error);
+              return Positioned.fill(
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Data Tidak Di Temukan',
+                        style: TextStyle(color: Colors.white),
+                      )));
+            }
+            return Positioned.fill(
+                child: Align(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            ));
+          },
+        )
+      ],
+    );
+    final body = Expanded(
+      child: FutureBuilder(
+          future: getJadwal(
+              location: _locationController.text.toLowerCase().toString()),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListJadwal(snapshot.data);
+            } else if (snapshot.hasError) {
+              print(snapshot.error);
+              return Center(child: Text('Data Tidak Di temukan'));
+            }
+
+            return Center(child: CircularProgressIndicator());
+          }),
+    );
+    return Scaffold(
+      body: Column(
+        children: <Widget>[header, body],
+      ),
+    );
+  }
+
+  _showDialogEditLocation(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Ubah Lokasi'),
+            content: TextField(
+              controller: _locationController,
+              decoration: InputDecoration(hintText: 'Cari Lokasi'),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: new Text('Batal',
+                      style: TextStyle(color: Colors.red, fontSize: 15))),
+              new FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context, () {
+                      setState(() {
+                        getJadwal(
+                            location: _locationController.text.toLowerCase().toString());
+                      });
+                    });
+                  },
+                  child: new Text(
+                    'Ok',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  )),
+            ],
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)),
+          );
+        });
+  }
+}
